2882c0b69aef4e592ce19fee8a6b479124b5c5d9
Michał Bysiewcz <michal.bysiewicz.kontakt@gmail.com>
2024-10-04 12:48:25 +0200

    Add swiper to ClientLayout

--- resources/js/Layouts/ClientLayout.vue
+++ resources/js/Layouts/ClientLayout.vue
@@ -1,11 +1,33 @@
 <script setup>
+  import { register } from 'swiper/element/bundle';
   import MainHeader from '../Partial/MainHeader.vue';
   import Footer from '../Partial/Footer.vue';
+
+  register();
 </script>
 
 <template>
   <div class="clientLayout">
     <MainHeader />
+    <div class="swiper">
+      <swiper-container
+        :centered-slides="true"
+        :pagination="true"
+        :slides-per-view="1"
+      >
+        <swiper-slide>
+          <div class="swiper__slide"><img alt="" src="https://krytykapolityczna.pl/wp-content/uploads/2023/02/kot.jpg"></div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="swiper__slide"><img alt="" src="https://krytykapolityczna.pl/wp-content/uploads/2023/02/kot.jpg"></div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="swiper__slide"><img alt="" src="https://krytykapolityczna.pl/wp-content/uploads/2023/02/kot.jpg"></div>
+        </swiper-slide>
+
+      </swiper-container>
+    </div>
+
     <div class="clientLayout__content">
       <slot />
     </div>
@@ -22,6 +44,13 @@
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+    gap: 24px;
+  }
+
+  .swiper__slide img {
+    height: 600px;
+    width: 100%;
+    object-fit: cover;
   }
 
   @media (min-width: 992px) {
