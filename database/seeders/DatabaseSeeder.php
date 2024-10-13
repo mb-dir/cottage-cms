b183aa3ca9c038725836d4995bb5d4eb3e5e8736
Michał Bysiewcz <michal.bysiewicz.kontakt@gmail.com>
2024-10-13 23:28:14 +0200

    Add page migration, model and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 
+use App\Models\Page;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -22,6 +23,37 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@onet.pl',
+        ]);
+
+        Page::factory()->create([
+            'name' => 'O nas',
+            'route_admin_name' => 'admin.about.index',
+            'route_client_name' => 'client.about.index',
+        ]);
+
+        Page::factory()->create([
+            'name' => 'Okolica i atrakcje',
+            'route_admin_name' => 'admin.attractions.index',
+            'route_client_name' => null,
+            'is_section' => true,
+        ]);
+
+        Page::factory()->create([
+            'name' => 'Galeria',
+            'route_admin_name' => 'admin.photo.index',
+            'route_client_name' => 'client.photo.index',
+        ]);
+
+        Page::factory()->create([
+            'name' => 'Pszczelarstwo',
+            'route_admin_name' => 'admin.beekeeping.index',
+            'route_client_name' => 'client.beekeeping.index',
+        ]);
+
+        Page::factory()->create([
+            'name' => 'Kontakt',
+            'route_admin_name' => 'admin.contact.index',
+            'route_client_name' => 'client.contact.index',
         ]);
     }
 }
