d3f6ee39c71ac8216d9ff2ebd6f4cc1fe0fd570f
Michał Bysiewcz <michal.bysiewicz.kontakt@gmail.com>
2024-11-04 21:32:41 +0100

    Create renderer component for content sections

---
+++ resources/js/Components/Renderers/ContentSectionRenderer.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+  const props = defineProps({
+    section: { required: true, type: Object },
+  });
+
+</script>
+
+<template>
+
+  <section class="section">
+    <h3>{{ section.title }}</h3>
+    <div v-html="section.content" />
+  </section>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Client/Beekeeping.vue
+++ resources/js/Pages/Client/Beekeeping.vue
@@ -1,5 +1,6 @@
 <script setup>
   import ClientLayout from '../../Layouts/ClientLayout.vue';
+  import ContentSectionRenderer from '../../Components/Renderers/ContentSectionRenderer.vue';
 
   defineProps({ contentSections: { type: Array, required: true } });
 </script>
@@ -7,32 +8,9 @@
 <template>
   <ClientLayout>
     <h2>Pszczoły to nasza pasja</h2>
-    <section class="section">
-      <h3 class="section__header">
-        Kochani goście
-      </h3>
-      <p class="section__text">Z racji tego, że nasza rodzina zajmuje się pszczołami od ponad 30 lat jesteśmy w satnie zaoferować Wam rożnego rodzaju dobra oferowane przez pszczoły takie jak:</p>
-      <ul class="section__list">
-        <li>Miody (w zależności od pory roku):
-          <ul>
-            <li>Miód wielokwiatowy</li>
-            <li>Miód spadziowy</li>
-            <li>Miód rzepakowy</li>
-            <li>Miód akacjowy</li>
-            <li>Miód lipowy</li>
-          </ul>
-        </li>
-        <li>Ziołomiody
-          <ul>
-            <li>Ziołomiód pokrzywowy</li>
-            <li>Ziołomiód z mięty</li>
-            <li>Ziołomiód z melisy</li>
-          </ul>
-        </li>
-        <li>Świece woskowe</li>
-      </ul>
-    </section>
-    {{ contentSections }}
+    <div v-if="contentSections.length > 0">
+      <ContentSectionRenderer v-for="section in contentSections" :section />
+    </div>
   </ClientLayout>
 </template>
 
