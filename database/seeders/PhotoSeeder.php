475cbd4bf8101ab30ee70c250b8ec10b4982eccc
Michał Bysiewcz <michal.bysiewicz.kontakt@gmail.com>
2024-12-09 09:08:34 +0100

    Create photos seeder with mock assets

---
+++ database/seeders/PhotoSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use App\Models\Photo;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+
+
+class PhotoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $sourceDir = public_path('photos/seeder');
+        $targetDir = public_path('storage/photos');
+
+        if (! File::exists($targetDir)) {
+            File::makeDirectory($targetDir, 0755, true);
+        }
+
+        $files = File::files($sourceDir);
+
+        foreach ($files as $file) {
+            $targetPath = $targetDir . '/' . $file->getFilename();
+
+            File::copy($file->getPathname(), $targetPath);
+
+            $relativePath = 'photos/' . $file->getFilename();
+
+            Photo::create([
+                'src' => $relativePath,
+            ]);
+        }
+
+        $this->command->info('Photos have been seeded successfully.');
+    }
+}
